fef3341d9706aa3dbb0f37dd180fd1e196c23be1
ellisk-l <ellis.keener.lacroix@gmail.com>
2023-01-28 16:37:42 -0400

    handled error, started on ui mockup

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use libatasmart::Disk;
 pub use libatasmart_sys::{SkDisk, SkSmartAttributeParsedData};
 use std::{fs, path::Path, ffi::c_void, ffi::CStr};
 
-extern "C" fn callback(d: *mut SkDisk, data: *const SkSmartAttributeParsedData, userdata: *mut c_void) {
-    unsafe {
-        let name = CStr::from_ptr((*data).name);
-        let name_str = name.to_str().unwrap();
-        println!("{name_str}");
-    }
-}
+// extern "C" fn callback(d: *mut SkDisk, data: *const SkSmartAttributeParsedData, userdata: *mut c_void) {
+//     unsafe {
+//         let name = CStr::from_ptr((*data).name);
+//         let name_str = name.to_str().unwrap();
+//         println!("{name_str}");
+//     }
+// }
 
-fn get_disk_attributes()
+// fn get_disk_attributes()
 
 fn get_disk_names() -> Vec<String> {
     let mut names: Vec<String> = Vec::new();
@@ -32,16 +32,18 @@ fn get_disks(disk_names: &Vec<String>) -> Vec<Disk> {
 
     for disk_name in disk_names{
         let full_name = disk_prefix.clone() + disk_name;
-        println!("{}", full_name);
-        let new_disk = Disk::new(Path::new(&(full_name))).unwrap(); //need to do error handling for permission denied/no support for smart
+        let new_disk = match Disk::new(Path::new(&(full_name))){
+            Ok(disk) => disk,
+            Err(_) => continue,
+        };
         disks.push(new_disk);
     }
-
     disks
 }
+
+
 
 fn main() {
     let disk_names = get_disk_names();
     let mut disks = get_disks(&disk_names);
-    println!("{}", disks[0].get_disk_size().unwrap());
 }
---
+++ src/ui.rs
@@ -0,0 +1,34 @@
+use cursive::align::HAlign;
+use cursive::traits::*;
+use cursive::views::{Dialog, DummyView, LinearLayout, TextView, Panel, Button};
+
+// This example uses a LinearLayout to stick multiple views next to each other.
+
+fn main() {
+    let mut siv = cursive::default();
+
+    let mut main_view = LinearLayout::horizontal();
+
+    let mut drive_list = LinearLayout::vertical();
+
+    for _ in 0..5 {
+        let button = Button::new("*", |s| s.quit());
+        let panel = Panel::new(button).title("/dev/sda");
+
+        drive_list.add_child(panel);
+    }
+
+    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac magna in velit interdum \
+     hendrerit id a quam. Aenean at diam vel odio auctor malesuada ut sed lectus. Fusce gravida, purus ut iaculis malesuada, diam est cursus risus, et rhoncus dui eros \
+     quis odio. Etiam cursus lobortis ante, vitae ullamcorper dui fermentum quis. Donec semper lacus sit amet placerat scelerisque. Praesent maximus justo quis justo \
+     sagittis semper. Sed eu vehicula turpis, id molestie velit. Quisque consectetur eget est sed rhoncus. Fusce consequat, sem nec placerat tincidunt, \
+     lorem elit eleifend augue, vitae vestibulum urna risus nec ipsum. Nunc nunc nisl, \
+    maximus sit amet sollicitudin et, mattis nec lectus. Praesent mollis orci nec mi imperdiet maximus.";
+
+    main_view.add_child(drive_list.scrollable());
+    main_view.add_child(TextView::new(text).max_width(20).scrollable());
+
+    siv.add_layer(main_view);
+
+    siv.run();
+}
